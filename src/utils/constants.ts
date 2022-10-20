85393c9ca960f0e73114bea9ef71621cf68892d3
Nawras Mansour <eng.nawrasma@gmail.com>
2022-10-20 04:57:12 +0400

    update the links to localhost

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,12 +1,12 @@
 
 //local
-// export const api_url = 'http://localhost:3000/api/users';
-// export const auth_url = 'http://localhost:3000/api/admins';
+ export const api_url = 'http://localhost:3000/api/users';
+ export const auth_url = 'http://localhost:3000/api/admins';
 
 
 // server
-export const api_url = 'http://crm.cala-store.com/api/users';
- export const auth_url = 'http://crm.cala-store.com/api/admins';
+//export const api_url = 'http://crm.cala-store.com/api/users';
+ //export const auth_url = 'http://crm.cala-store.com/api/admins';
 
 
 
